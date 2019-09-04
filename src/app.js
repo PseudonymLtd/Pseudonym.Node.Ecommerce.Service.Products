6143b8ff475403ea70d123428563adf9c9b9b8b2
Tom Day <Tom.Day@bjss.com>
2019-09-04 11:54:02 +0100

    Sessionless interaction

--- src/app.js
+++ src/app.js
@@ -5,4 +5,17 @@ const serviceRunner = new Framework.Service.Runner('Products Service');
 
 serviceRunner.RegisterController('/api', new ProductsController());
 
+serviceRunner.RegisterPostProcessor((request, response, complete) => {
+    return request.app.authenticator.Logout(request, err => {
+        if (err) {
+            request.app.logger.warn(`Error destroying session: ${err.toString()}`);
+            return complete(err);
+        }
+        else {
+            request.app.logger.info('Session Destroyed');
+            return complete();
+        }
+    })
+});
+
 serviceRunner.Start(3001);
